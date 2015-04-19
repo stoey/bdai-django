12fffad20f254ebc570b2ea56f6d4d28f2cc3336
Dan Head <dan@stoey.net>
2015-04-19 18:39:28 -0500

    Added page images to templates

--- pages/views.py
+++ pages/views.py
@@ -3,12 +3,17 @@ from django.http import Http404
 from django.shortcuts import render
 from django.template import Template
 from django.template import RequestContext
+from django.template.loader import get_template
 
 from .models import Page
 
 def view_page(request, path):
     try:
         page = Page.from_path(path)
+        content_template = get_template('page.html')
+        content = content_template.render(dict(
+            page=page,
+        ), request)
         editlink = None
         if request.user.is_staff:
             editlink = reverse('admin:pages_page_change', args=(page.pk,))
@@ -17,7 +22,7 @@ def view_page(request, path):
             page.directory.theme.template,
             dict(
                 title=page.title,
-                content=page.contents,
+                content=content,
                 editlink=editlink
             ),
         )
