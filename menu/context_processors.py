dfdab154fb6b5fd578088283057e2d10dd1596bc
Dan Head <dan@stoey.net>
2015-04-22 23:30:35 -0500

    Added menu items and sorted out the theme inheritance

--- menu/context_processors.py
+++ menu/context_processors.py
@@ -2,29 +2,57 @@ def menu(request):
     context = dict(
         menu_items=[
             dict(
-                name="Bleeding Disorders", 
+                name="Bleeding Disorders",
                 target="/bleeding-disorders",
-                active=False
+                active=False,
             ),
             dict(
-                name="Upcoming Events", 
+                name="Upcoming Events",
                 target="/events/",
-                active=False
+                active=False,
             ),
             dict(
-                name="Support Us", 
+                name="Support Us",
                 target="/support-bdai",
-                active=False
+                active=False,
+            ),
+            dict(
+                name="Camp",
+                target="/camp/",
+                active=False,
+            ),
+            dict(
+                name="What We Do",
+                target="/support/",
+                active=False,
+                children=[
+                    dict(
+                        name="Assistance Programs",
+                        target="/support/assistance/",
+                    ),
+                    dict(
+                        name="Community Support",
+                        target="/support/community/",
+                    ),
+                    dict(
+                        name="Advocacy",
+                        target="/support/advocacy",
+                    ),
+                    dict(
+                        name="Education",
+                        target="/support/education",
+                    ),
+                ]
             ),
         ],
     )
     if request.user.is_staff:
         context['menu_items'].append(dict(
-            name="Admin", 
+            name="Admin",
             target="/admin/",
             active=False
         ))
     for menu_item in context['menu_items']:
-        if request.path == menu_item['target']:
+        if request.path.startswith(menu_item['target']):
             menu_item['active'] = True
     return context
